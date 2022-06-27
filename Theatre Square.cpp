5110edcac314523696327d6e3abd0c1d38e96173
Salma Mamdoh <52897508+Salma-Mamdoh@users.noreply.github.com>
2022-06-27 18:57:56 +0200

    Add files via upload

---
+++ Theatre Square.cpp
@@ -0,0 +1,15 @@
+// Theatre Square.cpp :
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double n, m, a;
+	cin >> n>> m>> a;
+	int long long x;
+	x = ceil(m / a) * ceil(n / a);
+	cout << x;
+	
+}
+
