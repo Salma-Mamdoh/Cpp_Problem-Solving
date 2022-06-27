0102eeb931278efd7338c2cc264fcbf781ed40e6
Salma Mamdoh <52897508+Salma-Mamdoh@users.noreply.github.com>
2022-06-27 22:57:58 +0200

    Add files via upload

---
+++ Domino piling.cpp
@@ -0,0 +1,14 @@
+// Domino piling.cpp 
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double x, y,z;
+    cin >> x>> y;
+    z= floor((x * y) / 2);
+    cout << z;
+    
+}
+
